05c9ddc0e47e9ac9b065127155e42f128f98835a
Sathsarani <chandulasathsarani83@gmail.com>
2025-05-01 17:21:10 +0530

    Initial Commit

---
+++ src/main/java/com/automation/script1/Main.java
@@ -0,0 +1,28 @@
+package com.automation.script1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = WebDriverManager.chromedriver().create();
+       Thread.sleep(200);
+       driver.manage().window().maximize();
+        Thread.sleep(200);
+        driver.get("https://www.ebay.com/");
+        //driver.navigate().to("https://www.ebay.com/");
+        Thread.sleep(200);
+        driver.findElement(By.id("gh-ac")).click();
+        driver.findElement(By.id("gh-ac")).clear();
+        driver.findElement(By.id("gh-ac")).sendKeys("I phone");
+        Thread.sleep(300);
+        new Select(driver.findElement(By.id("gh-cat"))).selectByVisibleText("Cell Phones & Accessories");
+        Thread.sleep(300);
+        driver.findElement(By.xpath("//span[text()='Search']")).click();
+        Thread.sleep(3000);
+    }
+}
